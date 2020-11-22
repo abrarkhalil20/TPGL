5e56b00fe8f0cba701a8916e78e2a74e975fa05a
khalil <khalil@DESKTOP-LCPH39I>
2020-11-22 20:38:29 +0100

    First commit

---
+++ TPGL/src/ac/ma/esi/Test.java
@@ -0,0 +1,16 @@
+package ac.ma.esi;
+
+ 
+
+public class Test {
+
+ 
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World"); 
+    }
+
+ 
+
+}
